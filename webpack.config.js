100d99f10526520192544fcea86ec9d8c78ec1f9
Ivan <aw4ken1ngs@gmail.com>
2023-12-11 22:37:10 +0400

    Настройка сборки

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+const path = require('path');
+/* eslint-enable no-undef */
+
+/* eslint-disable no-undef */
+/* eslint-disable no-undef */
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+/* eslint-enable no-undef */
+/* eslint-enable no-undef */
+
+/* eslint-disable no-undef */
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ]
+  }
+};
