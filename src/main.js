1b9d3ab73350453ff519c8d6bddf717ce30dcbd9
Ivan <aw4ken1ngs@gmail.com>
2023-12-23 23:45:22 +0400

    Adding the presenter component and fixing errors

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
-import { render } from './render';
-import FilterView from './view/filter-view';
-import SortView from './view/sort-view';
-// import AddListPointView from './view/add-new-point-view';
-import ListView from './view/list-view';
-import EditPointView from './view/edit-point-view';
+import BoardPresenter from './presenter/board-presenter';
 
-const siteMainElement = document.querySelector('.trip-main');
-const siteSortElement = document.querySelector('.trip-events');
-
-render(new FilterView(), siteMainElement);
-render(new SortView(), siteSortElement);
-// render(new AddListPointView(), siteSortElement);
-render(new EditPointView(), siteSortElement);
-render(new ListView(), siteSortElement);
+document.addEventListener('DOMContentLoaded', () => {
+  const boardPresenter = new BoardPresenter();
+  boardPresenter.init();
+});
